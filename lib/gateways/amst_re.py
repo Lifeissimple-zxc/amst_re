50110ef0853da181250df236d55e20e8e90d71e0
Lifeissimple-zxc <mararkarp@gmail.com>
2023-09-30 12:19:34 +0200

    Introducing proxies because pararius seems to be blocking requests

--- lib/gateways/amst_re.py
+++ lib/gateways/amst_re.py
@@ -195,12 +195,17 @@ class FundaGateway(BaseGateway):
             next_url = f"{search_url}&search_result=2"
         return next_url
     
-    def perform_search(self, search_url: str):
+    def perform_search(self, search_url: str,
+                       debug_mode: Optional[bool] = None):
         """
         Performs a search on one search url (recursively reads different result pages)
         """
+        if debug_mode is None:
+            debug_mode = True
         main_logger.debug("Searching for %s", search_url)
         page_soup, e = self.fetch_page(url=search_url, features="html.parser")
+        if debug_mode:
+            main_logger.debug("Page soup for url %s: %s", search_url, page_soup) 
         if e is not None:
             main_logger.error("Failed to fetch %s", search_url)
             raise e
