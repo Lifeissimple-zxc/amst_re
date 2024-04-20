5d2dde902e553f200e3b1dc1ef8c6e3f30c72b86
Lifeissimple-zxc <mararkarp@gmail.com>
2024-04-20 23:06:48 +0200

    Added a hacky way of not running pararius without proxies

--- lib/utils.py
+++ lib/utils.py
@@ -10,6 +10,8 @@ def choose_gateway(url: str, pararius: amst_re.ParariusGateway,
     worker_class = None
     if "pararius" in url:
         worker_class = pararius
+        if not hasattr(worker_class, "proxy_list"):
+            return ValueError("no value in running pararius withou proxies")
     elif "funda" in url:
         worker_class = funda
     else:
