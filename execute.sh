cb6f3265a9d4ac15d6f5660c19ca54584e52fec7
Lifeissimple-zxc <mararkarp@gmail.com>
2024-08-03 13:04:31 +0200

    making ssl parsing conditional

---
+++ execute.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/mararkarp/projects/amst_re
+source env/bin/activate
+
+python main.py prod 0 none buy
+if [ $? -ne 0 ]; then
+    # only run with ssl if vanilla parsing fails
+    python main.py prod 0 ssl buy
+fi
+
+deactivate
